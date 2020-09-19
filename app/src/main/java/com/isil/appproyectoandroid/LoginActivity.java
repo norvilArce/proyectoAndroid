d28d97852a1e4b97312dc66622b7ff4291d40706
Jack Rackham <norvil.arce@gmail.com>
2020-09-19 01:49:32 -0500

    diseño de splash y login, queda diseño de botones

--- app/src/main/java/com/isil/appproyectoandroid/LoginActivity.java
+++ app/src/main/java/com/isil/appproyectoandroid/LoginActivity.java
@@ -2,13 +2,31 @@ package com.isil.appproyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnIngresar = (Button)findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        irAlMainActivity();
+    }
+
+    private void irAlMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
--- app/src/main/java/com/isil/appproyectoandroid/SplashActivity.java
+++ app/src/main/java/com/isil/appproyectoandroid/SplashActivity.java
@@ -2,7 +2,10 @@ package com.isil.appproyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                mostrarLoginActivity();
+            }
+        };
+
+        handler.postDelayed(runnable, 3000);
     }
+
+    private void mostrarLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
